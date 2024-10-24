21ed888ead82f4cf2cc6f94b52eb7f28629d1ef6
DimitrisApostolidis <marshalld135@gmail.com>
2024-10-24 20:19:44 +0300

    Version 0

---
+++ musicapp/src/main/java/Artist.java
@@ -0,0 +1,18 @@
+package com.musicapp.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String bio;
+    private String imageUrl;
+
+
+public class Artist {
+}
